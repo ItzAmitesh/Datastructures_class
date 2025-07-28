23c8422852ebcaed3affc42285f299a4b4c3bfd1
ItzAmitesh <143632584+ItzAmitesh@users.noreply.github.com>
2025-07-28 10:33:48 +0530

    Add files via upload

---
+++ program2.cpp
@@ -0,0 +1,11 @@
+//write  program to create an array of 10 integers and find the sum of all elements
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[10]={1,2,3,4,5,6,7,8,9,10},sum=0;
+    for(int i=0;i<10;i++){
+        sum+=arr[i];
+    }
+    cout<<"Sum of array elements: "<<sum<<endl;
+    return 0;
+}
